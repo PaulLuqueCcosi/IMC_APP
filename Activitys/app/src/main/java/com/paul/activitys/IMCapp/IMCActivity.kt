336be6e581554d2a7ead452fc02136a519760f24
PoolLuque <pluquec@unsa.edu.pe>
2024-01-28 15:24:47 -0500

    change focus in card male and female

--- Activitys/app/src/main/java/com/paul/activitys/IMCapp/IMCActivity.kt
+++ Activitys/app/src/main/java/com/paul/activitys/IMCapp/IMCActivity.kt
@@ -2,11 +2,72 @@ package com.paul.activitys.IMCapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import com.paul.activitys.R
 
 class IMCActivity : AppCompatActivity() {
+
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelecd: Boolean = false
+    private lateinit var viewMale :CardView
+    private lateinit var viewFemale : CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        initComponents()
+        initListeners()
+        initUX()
+
+
+    }
+
+    private fun initUX() {
+        setGenderColor()
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener{ selectMale()}
+        viewFemale.setOnClickListener { selectFemale()  }
+    }
+
+    private fun selectMale() {
+        isMaleSelected = true;
+        isFemaleSelecd = false;
+        setGenderColor()
+    }
+
+    private fun selectFemale() {
+        isMaleSelected = false;
+        isFemaleSelecd = true;
+        setGenderColor()
+    }
+
+    private fun setGenderColor() {
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelecd))
+    }
+
+    private fun changeGender() {
+
+    }
+
+
+    private fun initComponents() {
+        viewMale = findViewById(R.id.cvMale)
+        viewFemale = findViewById(R.id.cvFemale)
+    }
+
+    private fun getBackgroundColor(isSelectComponent: Boolean): Int {
+        val colorReference = if(isSelectComponent){
+            R.color.background_component_selected
+        } else{
+            R.color.background_component
+        }
+
+        return ContextCompat.getColor(this, colorReference)
+
     }
 }
