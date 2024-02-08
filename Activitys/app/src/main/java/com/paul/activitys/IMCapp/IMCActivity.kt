6c6d64382c386f5a2fb4cd53f27c3cc791c9a585
PoolLuque <pluquec@unsa.edu.pe>
2024-02-08 07:50:49 -0500

    V1.0 IMC app, complete interactions.

--- Activitys/app/src/main/java/com/paul/activitys/IMCapp/IMCActivity.kt
+++ Activitys/app/src/main/java/com/paul/activitys/IMCapp/IMCActivity.kt
@@ -1,17 +1,40 @@
 package com.paul.activitys.IMCapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.paul.activitys.R
+
 
 class IMCActivity : AppCompatActivity() {
 
     private var isMaleSelected: Boolean = true
     private var isFemaleSelecd: Boolean = false
-    private lateinit var viewMale :CardView
-    private lateinit var viewFemale : CardView
+    private var height: Int = 190
+    private var age: Int = 18
+    private var weight: Int = 60
+
+
+    private lateinit var textViewHeight: TextView
+    private lateinit var seekbarHeight: SeekBar
+    private lateinit var textViewWeight: TextView
+    private lateinit var textViewAge: TextView
+    private lateinit var btnPlusAge: FloatingActionButton
+    private lateinit var btnMinusAge: FloatingActionButton
+    private lateinit var btnPlusWeight: FloatingActionButton
+    private lateinit var btnMinusWeight: FloatingActionButton
+    private lateinit var btnCalculateIMC: Button
+
+
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,13 +47,78 @@ class IMCActivity : AppCompatActivity() {
 
     }
 
+
+
     private fun initUX() {
         setGenderColor()
+        setAge()
+        setWeight()
+        setHeight()
+    }
+
+    private fun setHeight() {
+        textViewHeight.setText(height.toString())
+        seekbarHeight.setProgress(height)
+
+    }
+
+    private fun setWeight() {
+        textViewWeight.text = weight.toString()
+    }
+
+    private fun setAge() {
+        textViewAge.text = age.toString()
     }
 
     private fun initListeners() {
-        viewMale.setOnClickListener{ selectMale()}
-        viewFemale.setOnClickListener { selectFemale()  }
+        viewMale.setOnClickListener { selectMale() }
+        viewFemale.setOnClickListener { selectFemale() }
+        btnPlusWeight.setOnClickListener { plusWeight() }
+        btnMinusWeight.setOnClickListener { minusWeight() }
+        btnPlusAge.setOnClickListener { plusAge() }
+        btnMinusAge.setOnClickListener { minusAge() }
+        btnCalculateIMC.setOnClickListener{ calculateIMC() }
+        seekbarHeight.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                height = progress
+                setHeight()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+//                Toast.makeText(this,"Seek bar progress is :$progressChangedValue",Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
+    }
+
+    private fun calculateIMC() {
+        val imc = weight / (height.toDouble() / 100 * height.toDouble() / 100)
+        Toast.makeText(this,  imc.toString(), Toast.LENGTH_LONG).show()
+    }
+
+    private fun minusAge() {
+        age--
+        setAge()
+    }
+
+    private fun plusAge() {
+        age++
+        setAge()
+    }
+
+    private fun minusWeight() {
+        weight--
+        setWeight()
+    }
+
+    private fun plusWeight() {
+        weight++
+        setWeight()
     }
 
     private fun selectMale() {
@@ -50,20 +138,28 @@ class IMCActivity : AppCompatActivity() {
         viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelecd))
     }
 
-    private fun changeGender() {
-
-    }
 
 
     private fun initComponents() {
         viewMale = findViewById(R.id.cvMale)
         viewFemale = findViewById(R.id.cvFemale)
+
+        textViewHeight = findViewById(R.id.tvHeight)
+        seekbarHeight = findViewById(R.id.sb_height)
+        textViewWeight = findViewById(R.id.tvWeight)
+        textViewAge = findViewById(R.id.tvAge)
+        btnPlusAge = findViewById(R.id.btnPlusAge)
+        btnMinusAge = findViewById(R.id.btnMinusAge)
+        btnPlusWeight = findViewById(R.id.btnPlusWeight)
+        btnMinusWeight = findViewById(R.id.btnMinusWeight)
+        btnCalculateIMC = findViewById(R.id.btnCalculateIMC)
+
     }
 
     private fun getBackgroundColor(isSelectComponent: Boolean): Int {
-        val colorReference = if(isSelectComponent){
+        val colorReference = if (isSelectComponent) {
             R.color.background_component_selected
-        } else{
+        } else {
             R.color.background_component
         }
 
